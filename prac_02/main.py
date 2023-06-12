98d5df6830124e98a00a06ea6bee6339d4261745
Fong Siu Man <siuman.fong@my.jcu.edu.au>
2023-06-12 15:25:58 +1000

    Refactor password check program to use functions

--- prac_02/main.py
+++ prac_02/main.py
@@ -1,18 +1,23 @@
-"""Module docstring"""
-
-
-# imports
-# CONSTANTS
 
 def main():
-    """Function docstring"""
-    # statements...
-    do_stuff()
+    mini_length_password = 6
+    password = get_password(mini_length_password)
+    print_star(password)
 
 
-def do_stuff():
-    """Function docstring"""
-    # statements...
+def print_star(password):
+    "print asterisks as long as the word"
+    for i in range(len(password)):
+        print("*", end='')
+
+
+def get_password(mini_length_password):
+    "get and check the password"
+    password = input("Input password:")
+    while len(password) < mini_length_password:
+        print("Invalid Password")
+        password = input("Input again:")
+    return password
 
 
 main()
