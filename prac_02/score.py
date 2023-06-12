f78829066594d0de883016d17d5ca96c4c976f64
Fong Siu Man <siuman.fong@my.jcu.edu.au>
2023-06-12 16:02:53 +1000

    Refactor determine score status program to use functions and add random score

---
+++ prac_02/score.py
@@ -0,0 +1,30 @@
+"""
+CP1404/CP5632 - Practical
+Broken program to determine score status
+"""
+import random
+
+
+def main():
+    """determine score status"""
+    score = float(input("Enter Score: "))
+    print(return_score_status(score))
+    random_score = random.randrange(0,100)
+    print("random_score:", random_score)
+    print(return_score_status(random_score))
+
+
+def return_score_status(score):
+    if score > 100:
+        return "Invalid score"
+    elif score >= 90:
+        return "Excellent"
+    elif score >= 50:
+        return "Passable"
+    elif score >= 0:
+        return "Bad"
+    else:
+        return "Invalid score"
+
+
+main()
