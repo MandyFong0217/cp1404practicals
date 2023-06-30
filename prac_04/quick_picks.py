f6fdde14f906da15c4a8c2a451d897ef358d5a78
Fong Siu Man <siuman.fong@my.jcu.edu.au>
2023-06-30 23:50:44 +1000

    add commit and use function

--- prac_04/quick_picks.py
+++ prac_04/quick_picks.py
@@ -1,38 +1,43 @@
-"""Module docstring"""
+"""
+"Quick Pick" Lottery Ticket Generator
+"""
 import random
 
 
-# imports
-# CONSTANTS
-
 def main():
-    """Function docstring"""
-    # statements...
-    CONSTANTS = []
-    line = []
     num_line = int(input('How many quick picks? '))
+    CONSTANTS = created_constants_list(num_line)
+    print_result(CONSTANTS, num_line)
+
+
+def print_result(CONSTANTS, num_line):
+    """This function print the Quick Pick Lottery Ticket Generator"""
+    for i in range(num_line):
+        for x in range(6):
+            # format the printing
+            print("{:2}".format(CONSTANTS[i][x]), end=' ')
+            if x == 5:
+                print('\n', end='')
+
+
+def created_constants_list(num_line):
+    """This function created a list of constants"""
+    line = []
+    CONSTANTS = []
     for i in range(num_line):
         for x in range(6):
             random_num = random.randrange(1, 45)
+            # check the number is not repeated in the line
             while random_num in line:
                 random_num = random.randrange(1, 45)
             line.append(random_num)
 
             if x == 5:
                 CONSTANTS.append(line)
+                # sorted the line
                 CONSTANTS[i].sort()
                 line = []
-
-    for i in range(num_line):
-        for x in range(6):
-            print("{:2}".format(CONSTANTS[i][x]), end=' ')
-            if x == 5:
-                print('\n', end='')
-
-
-def do_stuff():
-    """Function docstring"""
-    # statements...
+    return CONSTANTS
 
 
 main()
--- prac_04/total_income.py
+++ prac_04/total_income.py
@@ -7,17 +7,19 @@ Starter code for cumulative total income program
 def main():
     """Display income report for incomes over a given number of months."""
     incomes = []
-    number_0f_moths = int(input("How many months? "))
+    # 4. Refactor the months variable to a better name.
+    number_of_moths = int(input("How many months? "))
 
-    for month in range(1, number_0f_moths + 1):
+    for month in range(1, number_of_moths + 1):
+        # 3. Change to f-string
         income = float(input(f"Enter income for month {str(month)} : "))
         incomes.append(income)
 
-    print_report(incomes, number_0f_moths)
+    print_report(incomes, number_of_moths)
 
 
 def print_report(incomes, number_0f_moths):
-    """This function will print report"""
+    """This function will print Income report"""
     print("\nIncome Report\n-------------")
     total = 0
     for month in range(1, number_0f_moths + 1):
